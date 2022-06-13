247e30e8f24bb2a5aa87eff8d1e27ec5005c3aa0
KeshavKulhar <82043409+KeshavKulhar@users.noreply.github.com>
2022-06-13 10:36:28 +0530

    Add files via upload

---
+++ fruit.js
@@ -0,0 +1,32 @@
+img = "";
+Status = "";
+
+function preload() {
+    img = loadImage("fruit.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    imagedetector = ml5.objectDetector('cocossd',modelloaded)
+    document.getElementById("status").innerHTML = "Status - Detecting Object"
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    
+}
+
+function modelloaded() {
+    console.log("cocossd is initialized");
+    Status = true;
+    imagedetector.detect(img, gotresult);
+}
+
+function gotresult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+    }
+}
